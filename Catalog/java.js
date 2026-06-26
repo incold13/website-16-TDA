970d0949f02bdfe8e2b841e843d64c767b2f79ec
incold13 <incold13@gmail,com>
2026-06-26 02:43:26 +0200

    5 pls

--- Catalog/java.js
+++ Catalog/java.js
@@ -85,27 +85,41 @@ catalogJs.innerHTML = `
 
 let headerCatalog = document.getElementById('headerCatalog');
 let headerCatalogButton = document.getElementById('catalog');
+let headerCatalogButtonMobile = document.getElementById('catalogMobile');
 let headerCatalogStatus = false;
-
 let headerCatalogImg1 = document.getElementById('cartinka');
 let headerCatalogImg2 = document.getElementById('cartinka2');
-
 let headerCatalogImages = headerCatalogButton.querySelectorAll('img.list');
 
-headerCatalogButton.addEventListener('click', () => {
+function headerCatalogOpenClose() {
    headerCatalogStatus = !headerCatalogStatus;
 
    if (headerCatalogStatus) {
       headerCatalog.style.display = 'flex';
-      headerCatalogImg1.style.display = 'none';
-      headerCatalogImg2.style.display = 'flex';
+
+      if (headerCatalogImg1) {
+         headerCatalogImg1.style.display = 'none';
+      }
+
+      if (headerCatalogImg2) {
+         headerCatalogImg2.style.display = 'flex';
+      }
    } else {
       headerCatalog.style.display = 'none';
-      headerCatalogImg2.style.display = 'none';
-      headerCatalogImg1.style.display = 'flex';
+
+      if (headerCatalogImg2) {
+         headerCatalogImg2.style.display = 'none';
+      }
+
+      if (headerCatalogImg1) {
+         headerCatalogImg1.style.display = 'flex';
+      }
    }
 
    headerCatalogImages.forEach((img) => {
       img.classList.toggle('active');
    });
-});
+}
+
+headerCatalogButton.addEventListener('click', headerCatalogOpenClose);
+headerCatalogButtonMobile.addEventListener('click', headerCatalogOpenClose);
